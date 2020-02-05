acf17395cecb903f1c4615aecd973629a004422f
Dmitrii Kislenko <kislenko1910@mail.ru>
2020-02-05 19:34:40 +0200

    Navbar can be toggled

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
 menu.onclick = () => {
     let x = document.querySelectorAll('#nav-expanded')
-    console.log('x: ', x);
     x.forEach(element => {
-      if(element.className.indexOf("responsive")){
-        let newClass = element.replace(/responsive/g,'');
-        element.className = newClass
-        //   element.classNames += "responsive"
-         
-      } else {
-        //   element.classNames -= res
-        console.log(element.classNames)
-
-      }
+        element.classList.toggle('responsive') 
     });
 }
