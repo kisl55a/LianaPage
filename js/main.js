91499305f57300d618b8eaf453c852e44ed845bb
Dmitrii Kislenko <kislenko1910@mail.ru>
2020-02-08 12:08:08 +0200

    news are done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const clients = document.querySelector('#clients');
 const employees = document.querySelector('#employees');
 const users = document.querySelector('#users')
+const pieceOfNews = document.querySelectorAll('.piece-of-news')
+const newsDate = document.querySelectorAll('.news-date')
+let news = undefined;
 
 menu.onclick = () => {
     let x = document.querySelectorAll('#nav-expanded')
@@ -37,5 +40,36 @@ const counter = (max, id, time) => {
         current += Math.round(increase);
     }, 1000);
 }
+
+const newsFeed = () => {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = "https://www.lianatech.com/resources/blog.rss"; // site that doesn’t send Access-Control-*
+    fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+        .then(response => response.text())
+        .then(contents => {
+            let xml = contents;
+            let xmlDoc = new DOMParser().parseFromString(xml, 'text/xml');
+            for (let i = 0; i < pieceOfNews.length; i++) {
+                console.log('newsText.length: ', pieceOfNews.length);
+                let item = xmlDoc.getElementsByTagName("item")[i];
+                let description = item.getElementsByTagName("description")[0].innerHTML;
+                let link = item.getElementsByTagName("link")[0].innerHTML;
+                let date = new Date(item.getElementsByTagName("pubDate")[0].innerHTML);
+                let dateString = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
+                pieceOfNews[i].innerHTML = `
+                <a href="${link}">
+                    <div class="news-date">
+                         ${dateString}
+                    </div>
+                     <p class="news-text">
+                         ${description} 
+                        </p></a>`;
+            }
+    
+        })
+        .catch((err) => console.log(err))
+}
+newsFeed()
+
 
 
